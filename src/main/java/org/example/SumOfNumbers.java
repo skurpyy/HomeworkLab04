453b04fab0d50e7eb8cf79ad58b2c7105fec5b19
Angelo Costanza <acostanza1107@gmail.com>
2025-09-29 18:10:10 -0400

    Completed method to recursively print hello World Completed method to return sum of all numbers btwn n1 and n2 that are multiples of 7

--- src/main/java/org/example/SumOfNumbers.java
+++ src/main/java/org/example/SumOfNumbers.java
@@ -4,5 +4,23 @@ public class SumOfNumbers {
 
     public static void main(String[] args) {
 
+        int n1  = 10;
+        int n2 = 30;
+        int result = sumOfNumbers(n1,n2);
+        System.out.println("The sum of multiples of 7 between " + n1 + " and " + n2 + " is: " + result);
+
+    }
+
+    static int sumOfNumbers(int n1, int n2){
+        if (n1 > n2) {
+            return 0;
+        }
+
+        if (n1 % 7 == 0) {
+            return n1 + sumOfNumbers(n1 + 1, n2);
+
+        }else{
+            return sumOfNumbers(n1 + 1,n2);
+        }
     }
 }
